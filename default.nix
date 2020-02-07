cbf5632df8af52774c143534741d489a928db856
Christine Dodrill <me@christine.website>
2020-02-07 23:13:47 +0000

    add nix package stuff for niv usage

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.stdenv.mkDerivation rec {
+  name = "gruvbox-css";
+  version = "latest";
+  src = ./.;
+  phases = "installPhase";
+  installPhase = ''
+    mkdir -p $out
+    cp -rf $src/gruvbox.css $out/gruvbox.css
+  '';
+}
